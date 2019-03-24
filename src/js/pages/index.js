8264f4f37fa1deed2e05d7836a645c753b8137f0
dingdingding <DINGDINGDINGSOLA@163.com>
2019-03-24 15:31:11 +0800

    分类的日期完成

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,26 +1,71 @@
 define(['mui','picker'], (mui) => {
-	const list = document.querySelectorAll('.y-list span');
+	const yearType = document.querySelector('.year-type');
+	const dateType = document.querySelector('.date-type');
+
 	let picker = null;
+	let dtPicker = null;
+	let nowdate = {
+		nowY: new Date().getFullYear(),
+		nowM: new Date().getMonth() + 1
+	}
+	dateType.firstElementChild.innerHTML = nowdate.nowY +'-'+nowdate.nowM
 	const init = () => {
 		mui.init();
 		
 		picker = new mui.PopPicker();
 		 picker.setData([
 				 {value:'y',text:'年'},
-				 {value:'m',text:'月'},
-				 {value:'d',text:'日'}
+				 {value:'m',text:'月'}
+				
 			]);
-		 list[0].addEventListener('tap', addEvent);
+		 yearType.addEventListener('tap', addEvent);
+		  dtPicker = new mui.DtPicker({
+			  type: 'month',
+			  endDate: new Date()
+		  }); 
+		 
 	}
-	function addEvent(e) {	
-		const tar = e.target;
-		 picker.show(function (selectItems) {
-			 if(tar.tagName === 'SPAN') {
-				 tar.innerHTML = selectItems[0].text;
+	function addEvent() {	
+		picker.show(function (selectItems) {
+			// console.log(yearType.children)
+				 yearType.children[0].innerHTML = selectItems[0].text;
+			 if(selectItems[0].value === 'y') {
+				 dateType.firstElementChild.innerHTML = nowdate.nowY;
+			 } else {
+				 dateType.firstElementChild.innerHTML = nowdate.nowY +'-'+nowdate.nowM;
 			 }
-				console.log(selectItems[0].text);//智子
-				console.log(selectItems[0].value);//zz 
+				console.log(selectItems[0].text);
+				console.log(selectItems[0].value);
 		})
+		dateType.addEventListener('tap', () => {
+			let timetype = yearType.firstElementChild.innerHTML;
+			
+			if(timetype === '年') {	
+				changedate('none', '100%');
+			} else {
+				changedate('inline-block', '50%');
+			}
+			dtPicker.show(function (selectItems) { 
+				dateType.firstElementChild.innerHTML = 
+				timetype === '年'? selectItems.y.text: 
+				selectItems.y.text + '-' + selectItems.m.text;
+			    console.log(selectItems.y);//{text: "2016",value: 2016} 
+			    console.log(selectItems.m);//{text: "05",value: "05"} 
+			})
+		});
 	}
-	init()
+	
+
+	function changedate(s, w) {
+		let monthList = document.querySelector('div[data-id="picker-m"]');
+		let yearList = document.querySelector('div[data-id="picker-y"]');
+		let monthTitle = document.querySelector('h5[data-id="title-m"]');
+		let yearTitle = document.querySelector('h5[data-id="title-y"]');
+		
+		monthList.style.display = s;
+		monthTitle.style.display = s;
+		yearList.style.width = w;
+		yearTitle.style.width = w;
+	}
+	init() 
 })
